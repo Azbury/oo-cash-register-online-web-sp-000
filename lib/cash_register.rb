7117a84341514ed26da3f3b9c9b8b68d1dca3c1f
Austin Asbury <acasbury25@aol.com>
2019-08-29 17:33:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,8 @@
 class CashRegister
-  attr_reader :employee_discount
+  attr_reader :discount
 
   def initialize (employee_discount = "0")
     @total = 0
-    @employee_discount = employee_discount
+    @discount = employee_discount
   end
 end
