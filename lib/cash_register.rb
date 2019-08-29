75810d0733ca8c1e55b8c0a0d332e37edb1b4f78
Austin Asbury <acasbury25@aol.com>
2019-08-29 17:46:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,16 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :cart
   attr_reader :discount
 
   def initialize (employee_discount = 0)
     @total = 0
     @discount = employee_discount
+    @cart = []
   end
 
   def add_item (title, price, quantity = 1)
     self.total += price * quantity
+    self.cart << title
   end
 
   def apply_discount
